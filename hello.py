8914662a1a29004e71a1a53f633dd300d2559312
Jack <C23323661@mytudublin.ie>
2025-09-26 12:21:08 +0100

    Updated hello.py file with contact page and links

--- hello.py
+++ hello.py
@@ -4,5 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<p>Hello World!, I am a flask app</p>'
+	return "<h1>Home page</h1><a href='/about'>About</a> | <a href='/contact'>Contact</a>"
+	
+
+@app.route('/about')
+def about():
+	return '<p>This app is running on the flask web framework. <a href="https://flask.palletsprojects.com/">About flask</a></p>'
+
+@app.route('/contact')
+def contact():
+	return "<h1>Contact Page</h1><p>Email: C23323661@mytudublin.ie</p><a href='/'>Home</a> | <a href='/about'>About</a>"
 
