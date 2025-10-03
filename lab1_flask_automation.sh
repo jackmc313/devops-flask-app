482d491cb597a00ae68d735ae63dd8e25dbc80c9
Jack <C23323661@mytudublin.ie>
2025-10-03 15:25:10 +0100

    Week 3 lab added to repo

--- lab1_flask_automation.sh
+++ lab1_flask_automation.sh
@@ -1,13 +1,20 @@
-#!bin/bash
+#!/bin/bash
+
+
+cd /home/vagrant
 
 sudo apt update -y
 sudo apt upgrade -y
+sudo apt install -y python3-venv python3-pip
 
-sudo apt install nano vim python-is-python3 python3-venv python3-pip
-python -m  venv .my_venv
-source .myvenv/bin/activate
 
-pip install flask
+python3 -m venv .my_venv
+source .my_venv/bin/activate
+
+
+pip install --break-system-packages flask
+
+cd /vagrant
 
 cat <<EOF > hello.py
 from flask import Flask
@@ -16,13 +23,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<p>Hello World, I am a flask app|</p>'
+    return '<p>Hello World, I am a flask app!</p>'
 
 @app.route('/about')
 def about():
-	return '<p>This app is running on the flask web framework. <a href="https://flask.palletsprojects.com/">About flask</a>>/p>'
+    return '<p>This app is running on the flask web framework. <a href="https://flask.palletsprojects.com/">About flask</a></p>'
 EOF
 
-export FLASK_APP=hello.py 
-flask run --host=0.0.0.0
-
+export FLASK_APP=/vagrant/hello.py
+/home/vagrant/.my_venv/bin/flask run --host=0.0.0.0 &
